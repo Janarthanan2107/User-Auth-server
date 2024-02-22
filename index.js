60ab33be7ce54385bd240d1d29afcf30ebe757eb
Janarthanan2107 <janarthanan.v2107@gmail.com>
2024-02-22 20:53:08 +0530

    cookie parser

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { readFileSync } from "fs";
 
 // route
 import userAuthRouter from "./routes/userAuth.Route.js"
+import cookieParser from "cookie-parser";
 
 // configs
 dotenv.config();
@@ -16,9 +17,15 @@ const app = express();
 
 // primary middlewares
 // Enable CORS for all routes
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true, // Allow cookies with CORS
+    })
+);
 app.use(express.urlencoded({ limit: "500mb", extended: false }));
 app.use(express.json({ limit: "500mb" }));
+app.use(cookieParser());
 
 // MVC routes
 app.use("/api/v1/userAuth", userAuthRouter);
